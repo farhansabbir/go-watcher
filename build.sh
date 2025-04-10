b02a4dec97c13440fa3b735fb3d39d85f79de55d
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-04-10 13:45:06 -0400

    removed regular go build and invokes make file now from build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-export Version=`git rev-list -1 HEAD` && go build -o bin/fsswatcher -ldflags "-X main.Version=$Version" main.go
+# export Version=`git rev-list -1 HEAD` && go build -o bin/fsswatcher -ldflags "-X main.Version=$Version" main.go
+make all
