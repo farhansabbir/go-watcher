1d0b8b6a18da9ec6d4cd6d4e29cb12ee3cd2bfc9
Farhan Sabbir Siddique <fsabbir@gmail.com>
2025-04-10 13:39:25 -0400

    moved first timer run to separate function and added skip for ignore

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -19,12 +17,12 @@ import (
 type ignores []string
 
 var (
-	watch_location    string            = "./"
-	watch_delay_milli int               = 100
-	command           string            = ""
-	entries           map[string]string = make(map[string]string)
-	skips             ignores           = ignores([]string{})
-	Version                             = "0.0.1"
+	watch_location    string = "./"
+	watch_delay_milli int    = 100
+	// command           string            = ""
+	entries map[string]string = make(map[string]string)
+	skips   ignores           = ignores([]string{})
+	Version                   = "0.0.1"
 )
 
 func (i *ignores) Set(value string) error {
@@ -43,13 +41,40 @@ func init() {
 	flag.StringVar(&watch_location, "watch", "./", "location to watch for changes.")
 	flag.IntVar(&watch_delay_milli, "delay", 100, "delay in miliseconds between checking for changes.")
 	flag.Var(&skips, "ignore", "patterns to ignore when checking for changes, can use multiple times.")
-	flag.StringVar(&command, "command", "", "Parameterless command/script to run when changes are detected. Pass platform specific script for commands with arguments or multiple commands.")
+	// flag.StringVar(&command, "command", "", "Parameterless command/script to run when changes are detected. Pass platform specific script for commands with arguments or multiple commands.")
 	flag.Usage = func() {
 		fmt.Printf(`Usage: %s -watch <path> -delay <miliseconds>`, flag.CommandLine.Name())
 		fmt.Println()
 		fmt.Println("Version: " + Version)
 		flag.PrintDefaults()
 	}
+}
+
+func firstRun(rootpath string) {
+	var patterns string
+	for _, pattern := range skips {
+		patterns += pattern + "|"
+	}
+	patterns = strings.TrimRight(patterns, "|")
+	regex := regexp.MustCompile(patterns)
+	filepath.WalkDir(rootpath, func(path string, entry os.DirEntry, err error) error {
+		// check for error in walking path, skip dir if error
+		if err != nil {
+			if os.IsPermission(err) {
+				log.Fatal(err)
+			}
+			return filepath.SkipDir
+		}
+
+		// check if entry is to be skipped based on ignore patterns
+		if regex.Match([]byte(entry.Name())) && regex.String() != "" {
+			return filepath.SkipDir
+		}
+		entries[path] = lib.GetStringFromInfo(entry)
+		return nil
+	})
+	time.Sleep(time.Duration(watch_delay_milli) * time.Millisecond)
+	// log.Println("First run setup complete for " + rootpath)
 }
 
 func main() {
@@ -59,11 +84,12 @@ func main() {
 		os.Exit(1)
 	} else {
 		path, _ := filepath.Abs(stat.Name())
-		traverse(path)
+		firstRun(path)
+		watch(path)
 	}
 }
 
-func traverse(rootpath string) {
+func watch(rootpath string) {
 	log.Printf("Watching %s for changes.\n", rootpath)
 
 	var patterns string
@@ -72,7 +98,6 @@ func traverse(rootpath string) {
 	}
 	patterns = strings.TrimRight(patterns, "|")
 	regex := regexp.MustCompile(patterns)
-	first_run := true
 	for {
 		filepath.WalkDir(rootpath, func(path string, entry os.DirEntry, err error) error {
 			// check for error in walking path, skip dir if error
@@ -83,59 +108,19 @@ func traverse(rootpath string) {
 
 			// check if entry is to be skipped based on ignore patterns
 			if regex.Match([]byte(entry.Name())) && regex.String() != "" {
-				if first_run {
-					log.Println("Skipped " + entry.Name())
-				}
-				return nil
+				return filepath.SkipDir
 			}
-			//
-			//
-			// Check for changes
 			if val, exist := entries[path]; exist {
 				// entry exists in map, check if changed
 				if val != lib.GetStringFromInfo(entry) {
-					if command != "" {
-						log.Println("Change: '" + path + "'")
-						var procAttr os.ProcAttr
-						var output strings.Builder
-						procAttr.Files = []*os.File{os.Stdin, os.Stdout, os.Stderr}
-						// commandsplit := strings.Split(flag.Arg(0), " ")
-						ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-						defer cancel()
-						proc := exec.CommandContext(ctx, command)
-						proc.Stdout = &output
-
-						if err = proc.Run(); err != nil {
-							log.Println(err)
-							log.Println("Command '" + command + "' did not run successfully.")
-						} else {
-							fmt.Println(output.String())
-						}
-
-						// proc, _ := os.StartProcess(command, []string{command}, &procAttr)
-						// state, err := proc.Wait()
-						// if err != nil {
-						// 	log.Println(err.Error())
-						// }
-						// log.Println(state.Success())
-					} else {
-						log.Println("Change: '" + path + "'")
-					}
+					log.Println(path + " changed")
 					entries[path] = lib.GetStringFromInfo(entry)
 				}
 			} else {
-				if !first_run {
-					log.Println("New: '" + path + "'")
-				}
-
 				entries[path] = lib.GetStringFromInfo(entry)
 			}
 			return nil
 		})
-		// this only to be run for first run. Then set the first run false
-		if first_run {
-			first_run = false
-		}
 		time.Sleep(time.Duration(watch_delay_milli) * time.Millisecond)
 	}
 
